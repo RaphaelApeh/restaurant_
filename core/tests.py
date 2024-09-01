d3f827443dc43f90422bd0297cfb82dffe4c82d1
raphael apeh <raphaelapeh37@gmail.com>
2024-09-01 15:10:10 +0100

    remove github action

--- core/tests.py
+++ core/tests.py
@@ -1,4 +1,4 @@
-from django.test import TestCase
+from django.test import TestCase,Client
 from django.contrib.auth import get_user_model
 
 from .models import Food,Category
@@ -10,5 +10,24 @@ class UserTestCase(TestCase):
     def test_user_password(self):
         test_user = self.testuser.check_password('passworduser')
         self.assertTrue(test_user)
-    def test_user_count(self):
-        pass
+
+class FoodTestCase(TestCase):
+    def setUp(self):
+        test_category = Category.objects.create(name='Test',image='test.png')
+        category = test_category
+        self.food = Food.objects.create(
+            category=category,
+            name='test',
+            image='bg.png',)
+
+    def test_food_exists(self):
+        qs = Food.objects.all()
+        self.assertTrue(qs.exists())
+
+# class ViewTestCase(TestCase):
+#     def setUp(self):
+#         self.client = Client()
+
+    # def test_home_template(self):
+    #     path = self.client.get('/')
+    #     self.assertTemplateUsed(request,'index.html')
